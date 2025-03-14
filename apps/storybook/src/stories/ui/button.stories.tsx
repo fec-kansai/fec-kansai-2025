35faa20b41012cd96c60f2d9e12f4931f96cb6b8
okakyo <0622okakyo@gmail.com>
2025-03-14 15:14:44 +0900

    Storybook 環境の構築

---
+++ apps/storybook/src/stories/ui/button.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@workspace/ui/components/button";
+
+type ComponentType = typeof Button;
+type Story = StoryObj<ComponentType>;
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
+  render: (args) => <Button {...args}>テスト</Button>,
+} satisfies Meta<ComponentType>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    variant: "default",
+    size: "lg",
+  },
+};
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    size: "sm",
+  },
+};
