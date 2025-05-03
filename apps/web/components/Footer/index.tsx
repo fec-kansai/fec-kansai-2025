e618cd3606562c9ddfc119efff8d191358571481
mehm8128 <mehm8128@gmail.com>
2025-05-04 01:04:09 +0900

    fix: paddingや文字サイズを調整

--- apps/web/components/Footer/index.tsx
+++ apps/web/components/Footer/index.tsx
@@ -5,17 +5,17 @@ import { ExternalLinkIcon } from "@workspace/ui/components/icons/ui/ExternalLink
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-14 md:gap-12 py-12 px-20">
+    <footer className="flex flex-col gap-14 md:gap-12 py-12 px-6 md:px-20">
       <div className="flex flex-col gap-14 md:flex-row md:gap-0 justify-between">
         <SnsLinks />
         <FooterLinks />
       </div>
       <div className="flex flex-col md:flex-row gap-4 md:items-center md:gap-0 justify-between">
         <div className="flex-1 flex items-center gap-4 mx-auto md:mx-0">
-          <Text className="font-bold text-sm">運営団体</Text>
+          <Text className="font-bold text-xs md:text-sm">運営団体</Text>
           <a
             href="https://k3jp.org/"
-            className="text-[#C084FC] hover:text-[#A855F7] underline transition-duration-300 flex items-center gap-1 text-sm"
+            className="text-[#C084FC] hover:text-[#A855F7] underline transition-duration-300 flex items-center gap-1 text-xs md:text-sm"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,7 +27,7 @@ export default function Footer() {
             />
           </a>
         </div>
-        <div className="text-[#6B7280] text-sm">
+        <div className="text-[#6B7280] text-xs md:text-sm">
           © 2025 フロントエンドカンファレンス関西実行委員会. All rights
           reserved.
         </div>
