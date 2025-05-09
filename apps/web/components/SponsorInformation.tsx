6c215bf62b8df8d5ade09a0dc74c789c80de77f9
Hal <hal.spidernight@gmail.com>
2025-05-09 15:23:33 +0900

    fix: add docs link

--- apps/web/components/SponsorInformation.tsx
+++ apps/web/components/SponsorInformation.tsx
@@ -1,7 +1,8 @@
 import { Information } from "@workspace/ui/components/sponsor";
 
 function SponsorInformation() {
-  const SPONSOR_DOCS_LINK = "#";
+  const SPONSOR_DOCS_LINK =
+    "https://docs.google.com/presentation/d/1R7_pY5LdboES_HFNLobQLq44iw1A5F7TMzJ4OLyWCRo/edit?usp=sharing";
 
   return <Information docsLink={SPONSOR_DOCS_LINK} />;
 }
