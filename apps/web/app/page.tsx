23644df6c95198940e2e0b20889dc0c730a21d60
Hal <hal.spidernight@gmail.com>
2025-05-09 15:15:39 +0900

    fix: fix style

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,10 @@
 import MainVisual from "@/components/MainVisual";
-
+import { SponsorInformation } from "@/components/SponsorInformation";
 export default function Page() {
   return (
     <main className="flex-1">
       <MainVisual />
+      <SponsorInformation />
     </main>
   );
 }
