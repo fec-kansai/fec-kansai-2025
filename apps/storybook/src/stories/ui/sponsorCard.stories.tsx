c399c5ca888bd1ce9763b079c3f8f7f3a586cafb
Hal <hal.spidernight@gmail.com>
2025-05-09 16:51:32 +0900

    fix: fix layout, add stories

--- apps/storybook/src/stories/ui/sponsorCard.stories.tsx
+++ apps/storybook/src/stories/ui/sponsorCard.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { SponsorCard } from "@workspace/ui";
+import { Heading, SponsorCard } from "@workspace/ui";
 
 type ComponentType = typeof SponsorCard;
 type Story = StoryObj<ComponentType>;
@@ -14,7 +14,146 @@ export default meta;
 
 export const Default: Story = {
   args: {
-    href: "#",
+    href: "https://miidas.co.jp/",
     imageSrc: "/sponsors/miidas.webp",
+    alt: "ミイダス",
+  },
+};
+
+export const NotFoundImage: Story = {
+  args: {
+    href: "https://miidas.co.jp/",
+    imageSrc: "/sponsors/miidasaaaaaaa.webp",
+    alt: "ミイダス",
+  },
+};
+
+export const PremiumList: Story = {
+  render: () => {
+    const cardList = [
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "naniwa-premium",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "naniwa-premium",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "naniwa-premium",
+      },
+    ] as const;
+
+    return (
+      <div className="flex flex-col gap-5 my-6">
+        <Heading variant="secondary" className="text-center">
+          なにわプレミア
+        </Heading>
+        <ul className="flex align-center justify-center">
+          {cardList.map(({ href, imageSrc, alt, size }) => {
+            return (
+              <li key={alt} className="mx-6">
+                <SponsorCard
+                  href={href}
+                  imageSrc={imageSrc}
+                  alt={alt}
+                  size={size}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  },
+};
+
+export const GoldList: Story = {
+  render: () => {
+    const cardList = [
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+      {
+        href: "https://miidas.co.jp/",
+        imageSrc: "/sponsors/miidas.webp",
+        alt: "ミイダス",
+        size: "osaka-maido-gold",
+      },
+    ] as const;
+
+    return (
+      <div className="flex flex-col gap-5 my-6">
+        <Heading variant="secondary" className="text-center">
+          おおさかまいど
+        </Heading>
+        <ul className="flex flex-wrap gap-3 align-center justify-center">
+          {cardList.map(({ href, imageSrc, alt, size }) => {
+            return (
+              <li key={alt} className="mx-6">
+                <SponsorCard
+                  href={href}
+                  imageSrc={imageSrc}
+                  alt={alt}
+                  size={size}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   },
 };
--- packages/ui/src/components/sponsor/card.tsx
+++ packages/ui/src/components/sponsor/card.tsx
@@ -1,18 +1,41 @@
+import { type VariantProps, cva } from "class-variance-authority";
+import type * as React from "react";
+
+import { cn } from "@workspace/ui/lib/utils";
+
+const cardVariants = cva(
+  "bg-white rounded-lg p-5 hover:cursor-pointer overflow-hidden",
+  {
+    variants: {
+      size: {
+        "naniwa-premium": "w-[240px] h-[180px]",
+        "osaka-maido-gold": "w-[200px] h-[150px]",
+        "mini-takoyaki-silver": "w-[160px] h-[120px]"
+      },
+    },
+    defaultVariants: {
+      size: "naniwa-premium",
+    },
+  }
+);
+
+const cardImageVariants = "hover:opacity-50 hover:scale-110 duration-200 focus:scale-105 active:scale-95"
+
 interface SponsorCardProps {
   href: string,
-  imageSrc: string
+  imageSrc: string,
+  alt: string,
+  size: "naniwa-premium" | "osaka-maido-gold" | "mini-takoyaki-silver"
 }
 
-function SponsorCard(props: React.ComponentProps<"p">&SponsorCardProps){
+function SponsorCard(props: React.ComponentProps<"p">&SponsorCardProps & VariantProps<typeof cardVariants>){
 
-  const {className,href,imageSrc} = props
-
-  const classVariants = `bg-white rounded-xl p-5 ${className}`
+  const {className,href,imageSrc,size,alt} = props
 
   return (
-  <button className={classVariants}>
+  <button className={cn(cardVariants({ size, className }))}>
     <a href={href} target="_blank">
-      <img src={imageSrc}/>
+      <img className={cardImageVariants} src={imageSrc} alt={alt}/>
     </a>
   </button>
 )
