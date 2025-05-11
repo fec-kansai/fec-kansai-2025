6cef60ad6ebc9d333ac6febac62d50b208cb887e
Hal <hal.spidernight@gmail.com>
2025-05-11 23:21:04 +0900

    feat: add icons

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,10 +2,31 @@ import DateAndVenue from "@/components/DateAndVenue";
 import IgniteKansai from "@/components/IgniteKansai";
 import MainVisual from "@/components/MainVisual";
 import { SponsorInformation } from "@/components/SponsorInformation";
+import {
+  OctopusIcon,
+  TakoyakiBoxIcon,
+  TowerOfSunIcon,
+  TsuutenkakuIcon,
+} from "@workspace/ui";
+import styles from "assets/css/neon.module.css";
+
 export default function Page() {
   return (
-    <main className="flex-1">
+    <main className="flex-1 relative overflow-hidden">
       <MainVisual />
+      {/* icons */}
+      <div className="absolute left-[0%] top-[10%] w-[76px] md:w-[182px] animate-float5 rotate-20">
+        <TsuutenkakuIcon className={styles["neon-icon"]} />
+      </div>
+      <div className="absolute left-[10%] top-[50%] w-[76px] md:w-[182px] animate-float5 rotate-20">
+        <TowerOfSunIcon className={styles["neon-icon"]} />
+      </div>
+      <div className="absolute right-[3%] top-[15%] w-[76px] md:w-[182px] animate-float5 -rotate-10">
+        <OctopusIcon className={styles["neon-icon"]} />
+      </div>
+      <div className="absolute right-[8%] top-[70%] w-[76px] md:w-[182px] animate-float5 -rotate-20">
+        <TakoyakiBoxIcon className={styles["neon-icon"]} />
+      </div>
       <div className="mx-auto md:w-[1200px] w-full border-4 border-solid border-neon-light-blue box-shadow-neon-blue shadow-lg rounded-3xl my-8 p-4 bg-black/55 z-12">
         <div className="my-8">
           <IgniteKansai />
