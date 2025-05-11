2d4606474f67929b221fd3caaea9920243752436
Hal <hal.spidernight@gmail.com>
2025-05-11 21:02:44 +0900

    fix: remove unnecessary line-break

--- apps/web/components/Sponsor/Information.tsx
+++ apps/web/components/Sponsor/Information.tsx
@@ -5,15 +5,15 @@ interface SponsorInformationProps {
 }
 
 function SponsorInformation({ docsLink }: SponsorInformationProps) {
-  const INFORMATION_CONTENT = `フロントエンドカンファレンス関西では、共にイベントを盛り上げてくださるスポンサー企業様を募集しております。 \r\n
-協賛をご検討いただける皆さま向けに、イベントの概要やスポンサー特典、協賛プランなどをまとめた「スポンサー協賛資料」を公開しております。\r\n\r\n
-ぜひご一読の上、ご検討いただけますと幸いです。\r\n
+  const INFORMATION_CONTENT = `フロントエンドカンファレンス関西では、共にイベントを盛り上げてくださるスポンサー企業様を募集しております。
+協賛をご検討いただける皆さま向けに、イベントの概要やスポンサー特典、協賛プランなどをまとめた「スポンサー協賛資料」を公開しております。\r\n
+ぜひご一読の上、ご検討いただけますと幸いです。
 皆さまと共に、関西のフロントエンドコミュニティをさらに活性化できることを楽しみにしております。`;
 
   return (
     <section className="flex flex-col gap-6 items-center">
       <Heading>スポンサー</Heading>
-      <Text className="text-white leading-none whitespace-pre-wrap">
+      <Text className="text-white whitespace-pre-wrap">
         {INFORMATION_CONTENT}
       </Text>
       <Button className="my-3" variant="neon-red" asChild>
