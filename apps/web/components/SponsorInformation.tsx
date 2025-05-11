ac4bf26afce86b13f667b7a8ce44f4c2a5d57f53
Hal <hal.spidernight@gmail.com>
2025-05-11 19:22:02 +0900

    chore: change dir name

--- apps/web/components/SponsorInformation.tsx
+++ apps/web/components/SponsorInformation.tsx
@@ -1,5 +1,5 @@
 import { SPONSOR_DOCS_LINK } from "@/app/consts/test";
-import { SponsorInformation as Information } from "@/components/Sponsor";
+import { SponsorInformation as Information } from "@/components/s_ponsor";
 
 function SponsorInformation() {
   return <Information docsLink={SPONSOR_DOCS_LINK} />;
