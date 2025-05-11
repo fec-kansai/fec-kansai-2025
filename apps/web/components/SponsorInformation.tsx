d6095f5574ce83700cd0a4a18c881adc21832db1
Hal <hal.spidernight@gmail.com>
2025-05-11 19:22:27 +0900

    Revert "chore: change dir name"
    This reverts commit ac4bf26afce86b13f667b7a8ce44f4c2a5d57f53.

--- apps/web/components/SponsorInformation.tsx
+++ apps/web/components/SponsorInformation.tsx
@@ -1,5 +1,5 @@
 import { SPONSOR_DOCS_LINK } from "@/app/consts/test";
-import { SponsorInformation as Information } from "@/components/s_ponsor";
+import { SponsorInformation as Information } from "@/components/Sponsor";
 
 function SponsorInformation() {
   return <Information docsLink={SPONSOR_DOCS_LINK} />;
