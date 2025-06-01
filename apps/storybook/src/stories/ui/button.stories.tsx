dc0a89919697fbc660a2443686b88b4664f54a38
okakyo <0622okakyo@gmail.com>
2025-06-01 20:19:03 +0900

    fix: ボタンのサイズの修正

--- apps/storybook/src/stories/ui/button.stories.tsx
+++ apps/storybook/src/stories/ui/button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@workspace/ui";
+import { Button, Text } from "@workspace/ui";
 
 type ComponentType = typeof Button;
 type Story = StoryObj<ComponentType>;
@@ -8,37 +8,58 @@ const meta = {
   title: "Components/Button",
   component: Button,
   args: {
-    children: "Button",
+    children: "ボタン",
     variant: "neon-pink",
     size: "lg",
   },
   argTypes: {
     onClick: { action: "clicked" },
   },
+  parameters: {
+    layout: "padded",
+  },
 } satisfies Meta<ComponentType>;
 
 export default meta;
 
-export const Variants: Story = {
+export const Default: Story = {};
+
+export const Size: Story = {
+  render: () => {
+    return (
+      <>
+        <div className="flex flex-wrap gap-10">
+          <Button size="lg" className="w-30">
+            ボタン
+          </Button>
+          <Button className="w-30">ボタン</Button>
+          <Button size="sm" className="w-30">
+            ボタン
+          </Button>
+        </div>
+      </>
+    );
+  },
+};
+
+export const Color: Story = {
   render: () => {
     const variants = [
-      "neon-pink",
-      "neon-blue",
-      "neon-light-blue",
-      "neon-red",
-      "neon-green",
-      "neon-yellow",
+      { label: "ピンク", value: "neon-pink" },
+      { label: "青", value: "neon-blue" },
+      { label: "水色", value: "neon-light-blue" },
+      { label: "赤", value: "neon-red" },
+      { label: "緑", value: "neon-green" },
+      { label: "黄", value: "neon-yellow" },
     ] as const;
     return (
       <>
         {variants.map((variant) => (
-          <div key={variant} className="flex gap-2 mb-3">
-            <Button variant={variant} size="lg">
-              {variant}
-            </Button>
-            <Button variant={variant} size="sm">
-              {variant}
-            </Button>
+          <div key={variant.label} className="flex items-center gap-10 mb-6">
+            <div className="w-40">
+              <Text className="bold">カラー：{variant.label}</Text>
+            </div>
+            <Button variant={variant.value}>ボタン</Button>
           </div>
         ))}
       </>
