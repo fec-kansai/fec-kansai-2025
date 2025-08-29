7c44dd15741453c71554404f41d07d9b11047b87
okakyo <0622okakyo@gmail.com>
2025-08-29 15:24:45 +0900

    feat: sponsor の募集ボタンの削除

--- apps/web/components/Sponsor/Information.tsx
+++ apps/web/components/Sponsor/Information.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading, Text } from "@workspace/ui";
+import { Heading, Text } from "@workspace/ui";
 import { SponsorCard } from "./card";
 
 const INFORMATION_CONTENT =
@@ -7,45 +7,13 @@ const INFORMATION_CONTENT =
 ぜひご一読の上、ご検討いただけますと幸いです。
 皆さまと共に、関西のコミュニティをさらに活性化できることを楽しみにしております。` as const;
 
-interface SponsorInformationProps {
-  docsLink: string;
-  formLink: string;
-  isExpired?: boolean;
-}
-
-function SponsorInformation({
-  docsLink,
-  formLink,
-  isExpired,
-}: SponsorInformationProps) {
+function SponsorInformation() {
   return (
     <section id="sponsor" className="flex flex-col gap-6 items-center">
       <Heading>スポンサー</Heading>
       <Text className="text-white whitespace-pre-wrap">
         {INFORMATION_CONTENT}
       </Text>
-      <div className="flex flex-wrap gap-5 md:gap-20 justify-center my-0 md:mt-3 md:mb-10">
-        <Button
-          disabled={isExpired}
-          className="w-full sm:w-auto py-2.5 px-10"
-          variant="neon-red"
-          asChild
-        >
-          <a href={docsLink} target="_blank" rel="noreferrer">
-            スポンサー協賛資料
-          </a>
-        </Button>
-        <Button
-          disabled={isExpired}
-          className="w-full sm:w-auto py-2.5 px-10"
-          variant="neon-pink"
-          asChild
-        >
-          <a href={formLink} target="_blank" rel="noreferrer">
-            スポンサー募集フォーム
-          </a>
-        </Button>
-      </div>
       <Heading variant="secondary" className="text-center">
         会場スポンサー
       </Heading>
