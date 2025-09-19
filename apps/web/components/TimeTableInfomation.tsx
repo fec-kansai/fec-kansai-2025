4025cbfb52047512f9528d0f54e690e3263539bf
yuto ichinose <yuto.0526.sano@gmail.com>
2025-09-19 23:51:49 +0900

    fix: リンクを定数化

--- apps/web/components/TimeTableInfomation.tsx
+++ apps/web/components/TimeTableInfomation.tsx
@@ -1,5 +1,7 @@
 import { Button, Heading } from "@workspace/ui";
 
+const TIME_TABLE_LINK = "https://fortee.jp/fec-kansai-2025/timetable";
+
 export const TimeTableInfomation = () => {
   return (
     <section id="timetable" className="flex flex-col gap-10 items-center">
@@ -9,11 +11,7 @@ export const TimeTableInfomation = () => {
         variant="neon-pink"
         asChild
       >
-        <a
-          href={"https://fortee.jp/fec-kansai-2025/timetable"}
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href={TIME_TABLE_LINK} target="_blank" rel="noreferrer">
           タイムテーブル
         </a>
       </Button>
