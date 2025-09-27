21a25f84fcbb5282fc4e6e983eaa28f2d0320259
okakyo <0622okakyo@gmail.com>
2025-09-27 18:52:10 +0900

    fix: output の仕様修正

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  output: "export",
+  output: "standalone",
 };
 
 export default nextConfig;
