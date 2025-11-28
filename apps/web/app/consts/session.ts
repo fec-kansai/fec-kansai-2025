5795eccda71b888065d4363a070fe75a227639b4
jiyuujin <y-kitamura@wevnal.co.jp>
2025-11-28 14:40:59 +0900

    fix: 表示するよう切り替え

--- apps/web/app/consts/session.ts
+++ apps/web/app/consts/session.ts
@@ -2,4 +2,4 @@ export const formBaseUrl =
   "https://docs.google.com/forms/d/e/1FAIpQLSc-FPAjFZxOitEel_9xU5p--W5mnqICoEJzgOuMdtuz2cJdvQ/viewform?usp=header";
 export const fieldParam = "entry.1257886619";
 
-export const isStartedSurvey = false; // アンケートを開始する場合は true にする
+export const isStartedSurvey = true; // アンケートを開始する場合は true にする
